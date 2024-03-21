50ecabee1fd6e529843e524ef8057ee97798f056
Saltymelon13 <71376459+Saltymelon13@users.noreply.github.com>
2024-03-22 01:50:47 +0900

    Robert

---
+++ src/week_01/BetterString_R_W.java
@@ -0,0 +1,20 @@
+package week_01;
+
+import java.util.Random;
+
+public class BetterString_R_W {
+    public static void main(String ... args) {
+        System.out.println(randomString(-229985452)+' '+randomString(-147909649));
+    }
+
+    public static String randomString(int seed) {
+        Random rand = new Random(seed);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;;i++) {
+            int n = rand.nextInt(27);
+            if (n == 0) break;
+            sb.append((char) ('`' + n));
+        }
+        return sb.toString();
+    }
+}
