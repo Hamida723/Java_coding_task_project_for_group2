fa30dd8aa24474cdde597b3b381b139819fbd502
annagul <annaberdiyeva55@gmail.com>
2024-03-29 18:38:29 +0300

    Week01 tasks

--- src/week_01/First_Task_A_A.java
+++ src/week_01/First_Task_A_A.java
@@ -2,6 +2,62 @@ package week_01;
 
 public class First_Task_A_A {
     public static void main(String[] args) {
-        System.out.println("Hello Team");
+        System.out.println("---------NUMBERS-- ODD & EVEN--------");
+        oddorEven(6);
+        oddorEven(5);
+        oddorEven(-2);
+
+        System.out.println("------NUMBERS-- DIVIDE without OPERATOR-----");
+
+        divWithoutOP(25,5);
+        divWithoutOP(60,6);
+
+        System.out.println("------------FINRA------------");
+        //Method which prints nums 1 to 30
+        //multiple of 3 print "FIN"
+        //multiple of 5 print "RA"
+        //multiple both print "FINRA"
+        finra();
     }
+    public static void oddorEven (int num){
+        if (num > 0){
+            if (num % 2 == 0 ){
+                System.out.println("Num " + num + " is even");
+            } else  {
+                System.out.println("Num " + num + " is odd");
+            }
+        }
+        else {
+            System.out.println("Number is negative");
+        }
+    }
+    public static void  divWithoutOP (int dividend , int divisor){
+         int quotient = 0;
+         int orginalDivdend = dividend;
+
+            do {
+                dividend -= divisor;
+                quotient++;
+            }while (dividend >= divisor);
+            System.out.println(orginalDivdend + "/" + divisor + "=" + quotient);
+
+    }
+    public static void finra (){
+        String result ;
+        for (int i = 1; i <= 30; i++) {
+            result = "";
+            if(i % 3 == 0){
+                result += "FIN";
+            }
+            if (i % 5 == 0){
+                result += "RA";
+            }
+            if ( i % 3 == 0 & i % 5 == 0){
+                result += "FINRA";
+            }
+            System.out.print(result +" ");
+        }
+        System.out.println();
+    }
+
 }
